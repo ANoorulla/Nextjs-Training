85ece3ab4e27f4ba92a95e08d3e5a4d6bc41b272
Noorulla-paktolus <138501957+ANoorulla@users.noreply.github.com>
2024-07-10 17:00:05 +0530

    project-page-pre-rendering-and-data-fetching

---
+++ pages/events/index.js
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../helpers/api-util";
+
+import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
+
+function AllEventsPage(props) {
+  const router = useRouter();
+  const { events } = props;
+
+  function findEventHandler(year, month) {
+    const pullPath = `/events/${year}/${month}`;
+    router.push(pullPath);
+  }
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
+export default AllEventsPage;
