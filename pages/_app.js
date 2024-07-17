e4f39ff002c0e3319d46064c32ac14c2c5225e9e
Noorulla-paktolus <138501957+ANoorulla@users.noreply.github.com>
2024-07-17 13:16:45 +0530

    optimization nextjs app

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+import Layout from "../components/layout/layout";
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Next events</title>
+        <meta name="description" content="Next Js Events" />
+        <meta name="viewport" content="initial-scale-1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />;
+    </Layout>
+  );
+}
